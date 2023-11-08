36563ba97c65793d51f1fcca1d802b15d39fed65
Ryan Adi Putra <ryannadiputraa@gmail.com>
2023-11-08 17:02:28 +0800

    refactor: expense schema

--- internal/domain/expense.go
+++ internal/domain/expense.go
@@ -3,17 +3,19 @@ package domain
 import "time"
 
 type Expense struct {
-	ID        string          `json:"id" db:"id"`
-	Expense   string          `json:"expense" db:"expense"`
-	Amount    int             `json:"amount" db:"amount"`
-	Category  ExpenseCategory `json:"expense_category" db:"expense_category"`
-	Date      time.Time       `json:"date" db:"date"`
-	CreatedAt time.Time       `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time       `json:"updated_at" db:"updated_at"`
+	ID         string    `json:"id" db:"id"`
+	CategoryID string    `json:"category_id" db:"category_id"`
+	UserID     string    `json:"user_id" db:"user_id"`
+	Expense    string    `json:"expense" db:"expense"`
+	Amount     int       `json:"amount" db:"amount"`
+	Date       time.Time `json:"date" db:"date"`
+	CreatedAt  time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at" db:"updated_at"`
 }
 
 type ExpenseCategory struct {
 	ID       string `json:"id" db:"id"`
 	Category string `json:"category" db:"category"`
 	Ico      string `json:"ico" db:"ico"`
+	UserID   string `json:"user_id" db:"user_id"`
 }
